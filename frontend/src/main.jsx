035df7d424383433f4a6fd1a5d9cc9445e850509
zkrehabsphere-prog <zkrehabsphere@gmail.com>
2026-05-13 12:13:31 +0530

    fix: remove redundant HelmetProvider and fix potential rendering crash

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,6 @@ import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <App />
-      {/* <div className="text-4xl text-red-600 font-bold p-10">TEST RENDER - If you see this, React is working</div> */}
-    </HelmetProvider>
+    <App />
   </StrictMode>,
 )
